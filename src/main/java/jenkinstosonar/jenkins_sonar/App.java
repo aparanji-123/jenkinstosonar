863f845fb52e93a8760f8a4add3b7dea07cecd2c
APARANJI <APARANJI@LAPTOP-NVIS5OT0>
2022-11-25 11:46:19 +0530

    This is jenkins to sonar

---
+++ src/main/java/jenkinstosonar/jenkins_sonar/App.java
@@ -0,0 +1,13 @@
+package jenkinstosonar.jenkins_sonar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
